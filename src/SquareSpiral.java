e84dbd5849c351e0a834495d4b88ee56d3fe3f68
Finfinnatnicha <122598135+Finfinnatnicha@users.noreply.github.com>
2023-06-25 13:18:37 +0700

    Add files via upload

---
+++ src/SquareSpiral.java
@@ -0,0 +1,103 @@
+public class SquareSpiral
+{
+    public static void main(String[] args) 
+    //Drawing Pride Flag for Pride Month 
+    {
+        Turtle bob = new Turtle();
+        Turtle bob2 = new Turtle();
+        bob2.setPosition(0, -50);
+        Turtle bob3 = new Turtle();
+        bob3.setPosition(0, -100);
+        Turtle bob4 = new Turtle();
+        bob4.setPosition(0, -150);
+        Turtle bob5 = new Turtle();
+        bob5.setPosition(0, -200);
+        Turtle bob6 = new Turtle();
+        bob6.setPosition(0, -250);
+        Turtle bob7 = new Turtle();
+        bob7.setPosition(0, -250);
+
+        bob.penColor("red"); //red line
+        bob.forward(500);
+        bob.left(90);
+        bob.forward(50);
+        bob.left(90);
+        bob.forward(500);
+        bob.left(90);
+        bob.forward(50);
+        bob.left(90);
+        bob.forward(500);
+        bob.left(90);
+
+        bob2.penColor("orange"); //orange line
+        bob2.forward(500);
+        bob2.left(90);
+        bob2.forward(50);
+        bob2.left(90);
+        bob2.forward(500);
+        bob2.left(90);
+        bob2.forward(50);
+        bob2.left(90);
+        bob2.forward(500);
+        bob2.left(90);
+
+        bob3.penColor("yellow"); //yellow line
+        bob3.forward(500);
+        bob3.left(90);
+        bob3.forward(50);
+        bob3.left(90);
+        bob3.forward(500);
+        bob3.left(90);
+        bob3.forward(50);
+        bob3.left(90);
+        bob3.forward(500);
+        bob3.left(90);
+
+        bob4.penColor("green"); //green line
+        bob4.forward(500);
+        bob4.left(90);
+        bob4.forward(50);
+        bob4.left(90);
+        bob4.forward(500);
+        bob4.left(90);
+        bob4.forward(50);
+        bob4.left(90);
+        bob4.forward(500);
+        bob4.left(90);
+        
+        bob5.penColor("blue"); //blue line
+        bob5.forward(500);
+        bob5.left(90);
+        bob5.forward(50);
+        bob5.left(90);
+        bob5.forward(500);
+        bob5.left(90);
+        bob5.forward(50);
+        bob5.left(90);
+        bob5.forward(500);
+        bob5.left(90);
+
+        bob6.penColor("purple"); //purple line
+        bob6.forward(500);
+        bob6.left(90);
+        bob6.forward(50);
+        bob6.left(90);
+        bob6.forward(500);
+        bob6.left(90);
+        bob6.forward(50);
+        bob6.left(90);
+        bob6.forward(500);
+        bob6.left(90);
+
+        bob7.penColor("black"); //create pole
+        bob7.right(90);
+        bob7.forward(400);
+        bob7.left(90);
+        bob7.forward(20);
+        bob7.left(90);
+        bob7.forward(400);
+
+    }     
+    
+}
+    
